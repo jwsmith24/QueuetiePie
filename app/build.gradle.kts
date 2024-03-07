72411d1d21858e0924e0a71d14d7234fc48c7be9
Jake <jsmith181997@gmail.com>
2024-03-07 06:28:47 -1000

    Rename main class and remove generated init tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass.set("queuetiepie.App")
+    mainClass.set("queuetiepie.Main")
 }
 
 tasks.named<Test>("test") {
---
+++ app/src/main/java/queuetiepie/Main.java
@@ -0,0 +1,9 @@
+package queuetiepie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Get ready to queue!");
+    }
+    
+}
