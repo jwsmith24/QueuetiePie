fe726aee7a965740d028928449e280d9358a76a4
Jake <jsmith181997@gmail.com>
2024-03-07 11:07:15 -1000

    Add employee class and refactor POI methods to utilize the consolidated API

---
+++ app/src/main/java/queuetiepie/Employee.java
@@ -0,0 +1,14 @@
+package queuetiepie;
+
+public class Employee {
+
+    String name;
+    float totalBreakTime;
+
+
+    public Employee(String name, float totalBreakTime) {
+        this.name = name;
+        this.totalBreakTime = totalBreakTime;
+    }
+    
+}
---
+++ app/src/main/java/queuetiepie/ExcelHandler.java
@@ -0,0 +1,18 @@
+package queuetiepie;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class ExcelHandler {
+
+
+    
+
+    
+    
+}
